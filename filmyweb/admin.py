774264f77d9542b3d8d28d8c60306b5cbfeb0c73
Tobiasz Fuczek <tobfuc1412@gmail.com>
2024-03-22 21:26:52 +0100

    #4 Additional new path, changed in views files

--- filmyweb/admin.py
+++ filmyweb/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 from .models import Film
 # Register your models here.
 
-admin.site.register(Film)
+#admin.site.register(Film)
+
+@admin.register(Film)
+class FilmAdmin(admin.ModelAdmin):
+    #fields = ["title", "description", "year"]
+    #exclude = ["description"]
+    list_display = ["title", "imdb_rating", "year"]
+    list_filter = ["title"]
+    search_fields = ["title", "description"]
+
+
