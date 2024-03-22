0084740cde746df147fe5d04aefb8a2596acbe2c
Tobiasz Fuczek <tobfuc1412@gmail.com>
2024-03-22 20:48:29 +0100

    #3 Additional new models in class Film, new def

--- filmyweb/models.py
+++ filmyweb/models.py
@@ -1,3 +1,23 @@
+#Tu przechowujemy rzeczy które są w Naszej bazie danych. Np. Filmy, produkty etc.
 from django.db import models
 
-# Create your models here.
+class Film(models.Model):
+    title = models.CharField(max_length=32, blank=False, unique=True)
+    year = models.PositiveSmallIntegerField(default=2000)
+    description = models.TextField(default="")
+    premiere = models.DateField(null=True, blank=True)
+    imdb_rating = models.DecimalField(max_digits=4,decimal_places=2, null=True, blank=True)
+    image = models.ImageField(upload_to="images", null=True, blank=True)
+
+
+    def __str__(self):
+        return self.title_with_year()
+
+    def title_with_year(self):
+        return "{} ({})".format(self.title, self.year)
+
+
+
+
+
+
