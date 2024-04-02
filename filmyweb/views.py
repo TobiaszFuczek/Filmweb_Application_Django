55908dc8e4d75bd803f07783e4ba753580942f3c
Tobiasz Fuczek <tobfuc1412@gmail.com>
2024-04-02 22:15:53 +0200

    #16 Remove error: Cannot assign<bound method BaseModelForm.save of

--- filmyweb/views.py
+++ filmyweb/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 # Create your views here.
-from .models import Film, AdditionalInfo
+from .models import Film, AdditionalInfo, Rate
 from .forms import FilmForm, AdditionalInfoForm
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ObjectDoesNotExist
@@ -17,7 +17,8 @@ def new_film(request):
     form_additional = AdditionalInfoForm(request.POST or None)
     if all((form_film.is_valid(), form_additional.is_valid())):
         film = form_film.save(commit=False)
-        additional = form_additional.save
+        additional = form_additional.save(commit=False)
+        additional.save()
         film.additional = additional
         film.save()
         return redirect(all_films)
@@ -25,6 +26,8 @@ def new_film(request):
 @login_required()
 def edit_film(request, id):
     film = get_object_or_404(Film, pk=id)
+    rate = Rate.objects.filter(film=film)
+
     try:
         additional = AdditionalInfo.objects.get(film=film.id)
     except ObjectDoesNotExist:
@@ -33,11 +36,12 @@ def edit_film(request, id):
     form_additional = AdditionalInfoForm(request.POST or None, instance=additional)
     if all((form_film.is_valid(), form_additional.is_valid())):
         film = form_film.save(commit=False)
-        additional = form_additional.save
-        film.additional = additional
+        additional = form_additional.save(commit=False)
+        additional.film = film
+        additional.save()
         film.save()
         return redirect(all_films)
-    return render(request, 'film_form.html', {'form': form_film, 'form_additional': form_additional, 'nowy':False})
+    return render(request, 'film_form.html', {'form': form_film, 'form_additional': form_additional, 'rate': rate, 'nowy':False})
 @login_required()
 def delete_film(request, id):
     film = get_object_or_404(Film, pk=id)
